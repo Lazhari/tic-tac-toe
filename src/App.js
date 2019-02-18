8682e0f8949f4f5200a0171ef4554eb211f47e10
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-18 21:36:04 +0100

    Fixing the app title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Game from './Game';
-import './App.css';
+import Game from "./Game";
+import "./App.css";
 
 const App = props => {
-	return (
-		<div className="container">
-			<h1>Tik Tok Game</h1>
-			<Game />
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>Tic Tac Toe Game</h1>
+      <Game />
+    </div>
+  );
 };
 
 export default App;
